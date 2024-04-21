e801825c33544a8d2dcbdc5f5fcbfead21ed918a
Mathew Wicks <5735406+thesuperzapper@users.noreply.github.com>
2024-04-20 17:34:42 -0700

    fix dsl import in v2-compatible

--- pipeline_1/pipeline_v2_compatible.py
+++ pipeline_1/pipeline_v2_compatible.py
@@ -1,6 +1,9 @@
 from kfp import __version__ as kfp_version
-from kfp import compiler, dsl
-from kfp.v2 import dsl as v2_dsl
+from kfp import compiler
+
+# NOTE: we import this from `kfp.dsl`, as it's not under `kfp.v2.dsl`
+from kfp.dsl import PipelineExecutionMode
+from kfp.v2 import dsl
 
 #########################################################################################
 # Init
@@ -13,11 +16,11 @@ if int(kfp_version.split(".")[0]) != 1:
 #########################################################################################
 # Pipeline
 #########################################################################################
-@v2_dsl.component(base_image="python:3.10")
+@dsl.component(base_image="python:3.10")
 def step_1(
     intro_message: str,
     text: str,
-    output_file: v2_dsl.Output[v2_dsl.Artifact],
+    output_file: dsl.Output[dsl.Artifact],
 ):
     print(intro_message)
     print(text)
@@ -26,7 +29,7 @@ def step_1(
         f.write(text)
 
 
-@v2_dsl.pipeline(name="pipeline-1", description="pipeline-1 description")
+@dsl.pipeline(name="pipeline-1", description="pipeline-1 description")
 def pipeline_1(intro_message: str):
     step_1(intro_message=intro_message, text="1\n" * 100)
 
@@ -35,9 +38,19 @@ def pipeline_1(intro_message: str):
 # Main
 #########################################################################################
 def main():
-    compiler.Compiler(mode=dsl.PipelineExecutionMode.V2_COMPATIBLE).compile(
+    compiler.Compiler(mode=PipelineExecutionMode.V2_COMPATIBLE).compile(
         pipeline_func=pipeline_1,
         package_path="pipeline_v2_compatible.yaml",
+    )
+
+    from kfp_utils.client_manager import KFPClientManager
+
+    kfp_client_manager = KFPClientManager(
+        api_url="https://deploykf.example.com:8443/pipeline",
+        dex_username="user1@example.com",
+        dex_password="user1",
+        dex_auth_type="local",
+        skip_tls_verify=True,
     )
 
     # from kfp_utils.client_manager import KFPClientManager
